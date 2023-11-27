3e711774e05de66a6bf4ab52eaf83cc6f4b6b48f
Ani <desai.dhwani89@gmail.com>
2023-11-27 10:03:33 +0000

    initial commit

---
+++ src/test/java/com/capstone/museumapi/MuseumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.museumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
