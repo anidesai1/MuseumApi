bad142a38f672e256d16d535e5f67a93ff590023
Ani <desai.dhwani89@gmail.com>
2023-11-27 14:55:57 +0000

    added test to find all art- passed

---
+++ src/test/java/com/capstone/museumapi/controller/ArtTestsWithHttpRequest.java
@@ -0,0 +1,49 @@
+package com.capstone.museumapi.controller;
+
+import com.capstone.museumapi.model.Art;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Sql("classpath:test-data.sql")
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {"spring.sql.init.mode=never"})
+public class ArtTestsWithHttpRequest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper mapper;
+    ResultActions resultActions;
+    @Test
+    public void testGettingAllArt() throws Exception {
+        int expectedLength = 2;
+        ResultActions resultActions = this.mockMvc.perform(MockMvcRequestBuilders.get("/art")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        MvcResult result = resultActions.andReturn();
+        String contentAsAString = result.getResponse().getContentAsString();
+
+        Art[] art = mapper.readValue(contentAsAString, Art[].class);
+        assertAll("Testing from a test-data.sql file",
+                () -> assertEquals(expectedLength, art.length),
+                () -> assertEquals("Salisbury Cathedral from the Bishops Grounds", art[0].getName()),
+                () -> assertEquals("The Day Dream", art[1].getName()));
+
+    }
+}
